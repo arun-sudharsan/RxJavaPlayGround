7f5c0f55751f7692cf7ac9ec37495790b0b1a8d3
arun-sudharsan <arun.sudharsan@freshworks.com>
2019-05-21 23:11:57 +0530

    operator FromArray

--- app/src/main/java/io/arunbuilds/rxjavaplayground/MainActivity.kt
+++ app/src/main/java/io/arunbuilds/rxjavaplayground/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,38 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val datastream = Observable.just(1,2,3,4,5)
-        val dataObserver = object : Observer<Int>{
-            override fun onComplete() {
-                println("All the data has been received")
-            }
-
-            override fun onSubscribe(d: Disposable) {
-            println("Subscription: is Disposed? ${d.isDisposed}")
-            }
-
-            override fun onNext(t: Int) {
-            println("The data received is $t")
-            }
-
-            override fun onError(e: Throwable) {
-            println(e.localizedMessage)
-            }
-
+       val subscription =  createFromArray().subscribe{
+            arr-> println("Received array ${Arrays.toString(arr)}")
         }
-
-        datastream.subscribe(dataObserver)
     }
+
+    private fun createFromArray() = Observable.fromArray(arrayOf(1, 2, 3, 4, 5, 6))
 }
 
-
-/*
-Console Log:
-
-05-21 13:27:52.563 11881-11881/io.arunbuilds.rxjavaplayground I/System.out: Subscription: is Disposed? false
-05-21 13:27:52.563 11881-11881/io.arunbuilds.rxjavaplayground I/System.out: The data received is 1
-05-21 13:27:52.563 11881-11881/io.arunbuilds.rxjavaplayground I/System.out: The data received is 2
-05-21 13:27:52.563 11881-11881/io.arunbuilds.rxjavaplayground I/System.out: The data received is 3
-05-21 13:27:52.563 11881-11881/io.arunbuilds.rxjavaplayground I/System.out: The data received is 4
-05-21 13:27:52.563 11881-11881/io.arunbuilds.rxjavaplayground I/System.out: The data received is 5
-05-21 13:27:52.563 11881-11881/io.arunbuilds.rxjavaplayground I/System.out: All the data has been received*/
